a22946cbb7c2ce1e8e22b58ac72093eccf3dfa2a
jeldridgeTS <jeldridge@turbosquid.com>
2018-09-16 23:58:39 -0500

    add models for tags and join table model for taggings and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,5 +5,22 @@ class Article < ApplicationRecord
   enum status: { draft: 0, published: 1 }
 
   has_many :comments, dependent: :destroy
+  has_many :taggings
+  has_many :tags, through: :taggings
+
   validates_presence_of :title, :body, :thumb_image, :main_image
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+        Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
+
+  def self.tagged_with(name)
+    Tag.find_by_name!(name).articles
+  end
 end
