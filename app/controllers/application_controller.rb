6cc6da92e8c732afc1dd3c09e588c3a4c672d743
jeldridgeTS <jeldridge@turbosquid.com>
2018-11-12 21:33:13 -0600

    add pundit unauthorized rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelistConcern
   include SetVisitorOriginConcern
   include CurrentUserConcern
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
