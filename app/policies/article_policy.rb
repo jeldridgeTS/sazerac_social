53620a408289e28360d5dbbaf159fb1ef38b3f4d
jeldridgeTS <jeldridge@turbosquid.com>
2018-11-13 18:58:29 -0600

    lock down update/edit and toggle_publish_status actions

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -6,4 +6,12 @@ class ArticlePolicy < ApplicationPolicy
   def create?
     @user.writer? || @user.admin?
   end
+
+  def update?
+    user.present? && user.eql?(record.user)
+  end
+
+  def toggle_publish_status?
+    update? || user.admin?
+  end
 end
