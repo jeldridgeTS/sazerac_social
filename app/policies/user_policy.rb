648b8fb310e4ddb0370da34c709e9d2fa2ac2f38
jeldridgeTS <jeldridge@turbosquid.com>
2018-12-31 16:31:16 -0600

    add refactor comments

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,4 +1,6 @@
 class UserPolicy < ApplicationPolicy
+  # TODO: Possibly refactor and move admin methods to an admin policy
+  # ex: https://github.com/varvet/pundit#headless-policies
   def list_users?
     user.present? && user.admin?
   end
