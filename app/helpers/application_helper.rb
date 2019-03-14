b3c6c6aedf178988ba498f968257831113dd79dd
jeldridgeTS <jeldridge@turbosquid.com>
2019-03-13 23:02:17 -0500

    css is literally the worst, send help"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a? GuestUser
+      content_tag :div, class: "nav navbar-nav ml-auto w-100 justify-content-end" do
+        "<li>".html_safe +
+        (link_to "Register", new_user_registration_path, class: "btn btn-primary") + " ".html_safe +
+        (link_to "Login", new_user_session_path, class: "btn btn-primary") +
+        "</li>".html_safe
+      end
+    else
+      "<li>".html_safe +
+      link_to("Logout", destroy_user_session_path, method: :delete, class: "btn btn-primary") +
+      "</li>".html_safe
+    end
+  end
 end
