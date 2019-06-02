ec7063b316af5b4b623c59530d5c97bf16948ceb
jeldridgeTS <jeldridge@turbosquid.com>
2019-06-02 01:48:46 -0500

    add front end domain to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'localhost:4200'
-    origins '*'
+    origins 'http://localhost:3000', 'https://sazerac-social.herokuapp.com'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
