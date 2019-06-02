9a5536dd769083a9f81f0e7ec1432162d280bd7d
jeldridgeTS <jeldridge@turbosquid.com>
2019-06-02 01:55:21 -0500

    set cors credentials to true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3000', 'https://sazerac-social.herokuapp.com'
     resource '*',
       headers: :any,
+      credentials: true,
       methods: %i(get post put patch delete options head)
   end
 end
