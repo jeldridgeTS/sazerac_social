e777e23aeef827ed31f3b946c2adb19af663f034
jeldridgeTS <jeldridge@turbosquid.com>
2019-06-02 17:57:43 -0500

    remove commented out

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'localhost:4200'
     origins 'http://localhost:3000', 'https://sazerac-social.herokuapp.com'
     resource '*',
       headers: :any,
