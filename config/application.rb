3d158d47c385d855e0124522814059e8f955663b
jeldridgeTS <jeldridge@turbosquid.com>
2019-06-02 02:03:10 -0500

    remove restricted resource, set Access-Control-Allow-Origin header default for front end domain

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module SazeracSocial
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://sazerac-social.herokuapp.com'
+    }
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'localhost:4200'
     origins 'http://localhost:3000', 'https://sazerac-social.herokuapp.com'
-    resource '/api/v1/*',
+    resource '*',
       headers: :any,
       credentials: true,
       methods: %i(get post put patch delete options head)
