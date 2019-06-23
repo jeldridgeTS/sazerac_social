3fb629eea9d5c1a05d242905408c70a9088f2f36
jeldridgeTS <jeldridge@turbosquid.com>
2019-06-23 14:11:22 -0500

    first pass devise stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,22 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  # TEMP STUFF
+  # Devise code
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # TEMP STUFF
+
+  protected
+  # TEMP STUFF
+  # Devise methods
+  # Authentication key(:username) and password field will be added automatically by devise.
+  def configure_permitted_parameters
+    added_attrs = [:email, :display_name, :first_name, :last_name]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+  # TEMP STUFF
+
   private
 
   def user_not_authorized
