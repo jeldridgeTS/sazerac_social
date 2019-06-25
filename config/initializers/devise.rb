3a913276c8199a92ecb5767255d09ad3d757cd21
jeldridgeTS <jeldridge@turbosquid.com>
2019-06-24 22:53:39 -0500

    set devise parent controller to the api controller

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,6 +7,8 @@ Devise.setup do |config|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
   end
 
+  config.parent_controller = 'Api::ApiController'
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
