9796cc3589094453c0b2892d7e4fef9969819a6c
jeldridgeTS <jeldridge@turbosquid.com>
2019-06-25 22:56:35 -0500

    remove protect forgery and skip auth token checks

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,8 +1,6 @@
 class Api::ApiController < ActionController::Base
   respond_to :json
-  protect_from_forgery with: :exception
   before_action :authenticate
-  skip_before_action :verify_authenticity_token
 
   def authenticate
     unless logged_in?
--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
-  # Devise code
   skip_before_action :authenticate
-  skip_before_action :verify_authenticity_token
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
