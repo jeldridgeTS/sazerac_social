4166214a4226897c8a0f5e9e6e943c482f8f2bfc
jeldridgeTS <jeldridge@turbosquid.com>
2020-11-24 14:33:02 -0600

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://sazerac-social.herokuapp.com'
+    origins 'http://localhost:3000', 'https://sazerac-social.herokuapp.com', 'http://sazerac-social.herokuapp.com'
     resource '*',
       headers: :any,
       credentials: true,
