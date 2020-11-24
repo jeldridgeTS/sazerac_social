c0846d53c5b984cacbf5322f04e1bacfbf7d6c59
jeldridgeTS <jeldridge@turbosquid.com>
2020-11-24 15:12:47 -0600

    cors syntax

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-<<<<<<< HEAD
-    origins 'http://localhost:3000', 'https://sazerac-social.herokuapp.com', 'http://sazerac-social.herokuapp.com'
-||||||| merged common ancestors
-    origins 'http://localhost:3000', 'https://sazerac-social.herokuapp.com'
-=======
-    origins 'http://localhost:3001', 'https://sazerac-social.herokuapp.com'
->>>>>>> main
+    origins 'http://localhost:3001', 'https://sazerac-social.herokuapp.com', 'http://sazerac-social.herokuapp.com'
     resource '*',
       headers: :any,
       credentials: true,
