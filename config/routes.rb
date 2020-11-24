2301c814af916a29de5c66957e2665e3fd46349f
jeldridgeTS <jeldridge@turbosquid.com>
2020-11-24 15:17:31 -0600

    fix routes syntax

--- config/routes.rb
+++ config/routes.rb
@@ -3,36 +3,24 @@ Rails.application.routes.draw do
   # /api/v1
   scope module: :api, path: 'api', defaults: { format: :json } do
     scope module: :v1, path: 'v1' do
-<<<<<<< HEAD
       devise_for :users, controllers: {
-          registrations: 'api/v1/users/registrations',
-       }, skip: [:sessions, :password]
+        registrations: 'api/v1/users/registrations',
+      }, skip: [:sessions, :password]
     end
-  end
-||||||| merged common ancestors
-      devise_for :users, controllers: {
-           registrations: 'api/v1/users/registrations',
-       }, skip: [:sessions, :password]
+
+    devise_for :users, controllers: { registrations: 'api/v1/users/registrations' }, skip: [:sessions, :password]
+
+    resource :sessions, :only => [:create, :destroy]
+    get 'logged_in', to: 'sessions#logged_in'
+    get 'current_user', to: 'current_user#show', as: 'current_user'
+
+    resources :articles, except: [:new, :edit] do
+      resources :comments, except: [:new, :edit]
+      member { post :toggle_publish_status }
     end
-  end
-=======
->>>>>>> main
 
-      devise_for :users, controllers: { registrations: 'api/v1/users/registrations' }, skip: [:sessions, :password]
-
-      resource :sessions, :only => [:create, :destroy]
-      get 'logged_in', to: 'sessions#logged_in'
-      get 'current_user', to: 'current_user#show', as: 'current_user'
-
-      resources :articles, except: [:new, :edit] do
-        resources :comments, except: [:new, :edit]
-        member { post :toggle_publish_status }
-      end
-
-      get 'articles/tags/:tag', to: 'articles#index', as: 'article_tag'
-      get 'article/:id', to: 'articles#show', as: 'article_show'
-
-    end
+    get 'articles/tags/:tag', to: 'articles#index', as: 'article_tag'
+    get 'article/:id', to: 'articles#show', as: 'article_show'
   end
 
   namespace :admin do
