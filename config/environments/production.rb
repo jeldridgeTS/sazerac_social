1f59f66dbea1a013913ee5dea97553f4dc942ff8
jeldridgeTS <jeldridge@turbosquid.com>
2020-12-03 15:18:30 -0600

    default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "https://sazeracsocial-api.herokuapp.com" }
+  Rails.application.routes.default_url_options = { host: "https://sazeracsocial-api.herokuapp.com" }
 end
